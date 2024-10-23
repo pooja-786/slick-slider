ac38586d007b945a8f475b5e46d10bf7b9b156da
pooja-786 <pjagodara13@gmail.com>
2024-10-23 15:39:30 +0530

    add page

--- asstes/slick-slider.js
+++ asstes/slick-slider.js
@@ -3,13 +3,13 @@
           arrows: false,
           dots:true,
   slidesToShow: 3,
-  slidesToScroll: 3,
+  slidesToScroll: 1,
            responsive: [
     {
       breakpoint: 1024,
       settings: {
         slidesToShow: 3,
-        slidesToScroll: 3,
+        slidesToScroll: 1,
         infinite: true,
         dots: true
       }
@@ -18,7 +18,7 @@
       breakpoint: 600,
       settings: {
         slidesToShow: 2,
-        slidesToScroll: 2
+        slidesToScroll: 1
       }
     },
     {
@@ -33,6 +33,36 @@
     // instead of a settings object
   ]
 });
-        });
+    });
+        
+
+    
+$('.slick-slider').slick({
+  centerMode: true,
+  centerPadding: '60px',
+  slidesToShow: 1.66,
+  dots: true,
+   arrows:false,
+  responsive: [
+    {
+      breakpoint: 768,
+      settings: {
+        arrows: false,
+        centerMode: true,
+        centerPadding: '40px',
+        slidesToShow: 1.66
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        arrows: false,
+        centerMode: true,
+        centerPadding: '40px',
+        slidesToShow: 1.66
+      }
+    }
+  ]
+});
     
 	
